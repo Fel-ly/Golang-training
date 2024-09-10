9264c949d42f09375ea460d88df86ed5955ee108
Fel-ly <kiokofpm@gmail.com>
2024-09-10 16:39:17 +0300

    question 3

--- Exercises/exercise1.go
+++ Exercises/exercise1.go
@@ -1,19 +1,23 @@
 package main
 
 import "fmt"
+import "math"
 
 func main(){
+	//  Question 1
 	i := 50
 	f := float64(i)
 
 	fmt.Printf("Integer i is %d\n", i)
 	fmt.Printf("Floating point f is %f\n\n", f)
 
-	// calling the question2 function
-	question2()
+	
+	question2() // calling the question2 function
+	question3() // calling the question3 function
+
 }
 
-//Question 2
+// Question 2
 func question2(){
 	const value = 16
 
@@ -21,6 +25,21 @@ func question2(){
 	f := value
 
 	fmt.Printf("Integer value i is %d\n", i)
-	fmt.Printf("Floating-point value f is %f\n", f)
+	fmt.Printf("Floating-point value f is %f\n\n", f)
 }
 
+//  Question 3
+func question3(){
+	var b byte = math.MaxUint8
+	var smallI int32 = math.MaxInt32
+	var bigI uint64 = math.MaxUint64
+
+	b += 1
+	smallI += 1
+	bigI += 1
+
+	fmt.Printf("b (byte) = %v\n", b)
+	fmt.Printf("smallI (int32) = %v\n", smallI)
+	fmt.Printf("bigI (uint64) = %v\n", bigI)
+
+}
