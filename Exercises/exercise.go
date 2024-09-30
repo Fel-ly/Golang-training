8917af68e560d6b34e9443e747267a3b1aaab071
Fel-ly <kiokofpm@gmail.com>
2024-09-30 12:41:42 +0300

    exercise 1 update

--- Exercises/exercise.go
+++ Exercises/exercise.go
@@ -4,6 +4,12 @@ import "fmt"
 import "math"
 
 func main(){
+
+	exercise1()
+}
+
+//Exercise 1
+func exercise1(){
 	//  Question 1
 	i := 50
 	f := float64(i)
@@ -11,25 +17,20 @@ func main(){
 	fmt.Printf("Integer i is %d\n", i)
 	fmt.Printf("Floating point f is %f\n\n", f)
 
-	
-	question2() // calling the question2 function
-	question3() // calling the question3 function
 
-}
+	// Question 2
 
-// Question 2
-func question2(){
 	const value = 16
 
-	i := value
-	f := value
+	int := value
+	flt := value
 
-	fmt.Printf("Integer value i is %d\n", i)
-	fmt.Printf("Floating-point value f is %f\n\n", f)
-}
+	fmt.Printf("Integer value i is %d\n", int)
+	fmt.Printf("Floating-point value f is %f\n\n", flt)
 
-//  Question 3
-func question3(){
+
+	//  Question 3
+
 	var b byte = math.MaxUint8
 	var smallI int32 = math.MaxInt32
 	var bigI uint64 = math.MaxUint64
@@ -41,5 +42,4 @@ func question3(){
 	fmt.Printf("b (byte) = %v\n", b)
 	fmt.Printf("smallI (int32) = %v\n", smallI)
 	fmt.Printf("bigI (uint64) = %v\n", bigI)
-
 }
