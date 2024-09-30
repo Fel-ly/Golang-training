5dc4411d56777af63a1357f4b6e180f96003cef6
Fel-ly <kiokofpm@gmail.com>
2024-09-30 15:01:23 +0300

    pointers to structs

--- Lessons/Session4/session4.go
+++ Lessons/Session4/session4.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 //creating a struct
+// Note: structs are created outside the main function for re-usability
 type Person struct{
 	Name string
 	Age int
@@ -14,10 +15,19 @@ type Address struct{
 	Street string
 }
 
+type Calculator struct{  //Structs with Functions as Fields
+	Add func(a, b int) int
+}
+
+func updateAge(p *Person, newAge int){	//Pointers to Structs
+
+	p.Age = newAge
+}
+
 func main(){
 	// using  a struct created
 	person := Person{
-		Name: "Felly",
+		Name: "Jane",
 		Age: 21,
 		Gender: "Female",
 		Address: Address{ //nested structs
@@ -29,7 +39,23 @@ func main(){
 	fmt.Println(person) //prints all properties
 	fmt.Println(person.Name, "is a", person.Age, "year old", person.Gender)
 
-	fmt.Println(person.Name, "is a", person.Age, "year old", person.Gender, "who lives in", person.Address.City, "city,", person.Address.Street, "street.")
+	person.Age = 25// changing a value
+	fmt.Println(person.Name, "is a", person.Age, "year old", person.Gender, "who lives in", person.Address.City, "city,", person.Address.Street, "street.\n")
 
 
+	//Structs with Functions as Fields
+	calc := Calculator{
+		Add: func(a, b int) int{
+			return (a + b)
+		},
+	}
+
+	result := calc.Add(20, 3)
+	fmt.Println("The sum is", result, "\n")
+
+
+	//Pointers to Structs
+	updateAge(&person, 20)
+	fmt.Println(person.Age)
+	fmt.Println(person.Name, "is a", person.Age, "year old", person.Gender)
 }
